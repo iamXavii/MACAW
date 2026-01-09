ebc7964424624122166b1cc9d5941e739d29fb01
Xavi <oscarxavier.santander@gmail.com>
2026-01-08 23:14:20 -0400

    Conectar frontend a backend de Vercel

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3001/api',
+    baseURL: 'https://macaw-ffwc.vercel.app/api',
 });
 
 // Interceptor para agregar el token a las peticiones
